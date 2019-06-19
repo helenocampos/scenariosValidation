6e296f911b4348211870bab8d602ab6edd024d62
Heleno Campos <camposheleno0@gmail.com>
2019-06-19 17:51:55 -0300

    base

---
+++ src/main/java/info/heleno/DataValidation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.heleno;
+
+/**
+ *
+ * @author helenocampos
+ */
+public class DataValidation{
+  String[] chars = {"@"};
+
+  public boolean isValidUsername(String username){
+      if (username == null) return false;
+      return username.length() < 20 && username.length() > 0;
+  }
+  
+  public boolean isValidEmail(String email){
+    if(email.contains(chars[0])){
+      return true;
+    }
+    return false;
+  }
+}
---
+++ src/test/java/DataValidationTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import info.heleno.DataValidation;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author helenocampos
+ */
+public class DataValidationTest
+{
+    
+    DataValidation validation;
+    
+    public DataValidationTest()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+        validation = new DataValidation();
+    }
+
+
+     @Test
+     public void validEmail() {
+         assertEquals(true,validation.isValidEmail("test@test.com"));
+     }
+     
+     @Test
+     public void invalidEmail() {
+         assertEquals(false,validation.isValidEmail("test.com"));
+     }
+}
